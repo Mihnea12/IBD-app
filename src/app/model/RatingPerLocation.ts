0136585779d62931af43bf7030f0793c51718227
BiancaToma <tomabianca1505@gmail.com>
2023-01-15 18:31:51 +0200

    General Statistics

---
+++ src/app/model/RatingPerLocation.ts
@@ -0,0 +1,9 @@
+export class RatingPerLocation {
+  location: string;
+  rating: number;
+
+  constructor(location:string, rating: number) {
+    this.location = location;
+    this.rating = rating;
+  }
+}
