714234e3fd6c4be6aed065320474482aa6d04d1d
Mihnea <mihnea.pascu12@gmail.com>
2023-01-15 15:38:13 +0200

    add review button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,15 @@ import { AppComponent } from './app.component';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { GoogleMapComponent } from './google-demo/google-demo.component'
 import { AppRoutingModule } from './app-routing.module';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { DlDateTimeDateModule, DlDateTimePickerModule } from 'angular-bootstrap-datetimepicker';
+
 
 @NgModule({
   declarations: [
@@ -13,7 +22,17 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     GoogleMapsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatRippleModule,
+    DlDateTimeDateModule,  // <--- Determines the data type of the model
+    DlDateTimePickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
