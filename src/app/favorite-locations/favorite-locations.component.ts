e54a7fa05c125369a9eccb56a6669c0a7202acf9
Mihnea <mihnea.pascu12@gmail.com>
2023-01-16 21:12:32 +0200

    add table for favorite locations

--- src/app/favorite-locations/favorite-locations.component.ts
+++ src/app/favorite-locations/favorite-locations.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { Location } from '../model/Location';
 import { AppService } from '../service/app.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-favorite-locations',
@@ -10,13 +12,26 @@ import { AppService } from '../service/app.service';
 export class FavoriteLocationsComponent {
 
     favoriteLocations: Location[]
+    dataSource : MatTableDataSource<Location>;
+    selectedRowIndex:any;
+    displayedColumns: string[] = ['idLocation', 'nameLocation', 'longitude', 'latitude', 'locationType', 'rating' ];
 
-    constructor(private appService: AppService) {
+    constructor(private appService: AppService, private router: Router) {
         this.favoriteLocations = []
+        this.favoriteLocations.push(new Location('id1','locatie',1,2,'tip',5))
+        this.favoriteLocations.push(new Location('id2','locatie',1,2,'tip',5))
+        this.favoriteLocations.push(new Location('id3','locatie',1,2,'tip',5))
+        this.favoriteLocations.push(new Location('id4','locatie',1,2,'tip',5))
+        this.favoriteLocations.push(new Location('id5','locatie',1,2,'tip',5))
+        this.dataSource = new MatTableDataSource(this.favoriteLocations)
     }
     ngOnInit() {
         this.appService.getLocations().subscribe(response => {
             console.log(response)
         })
+    }
+    redirect(location: Location) {
+        console.log(location)
+        this.router.navigate(['/location/' + location.locationId])
     }
 }
